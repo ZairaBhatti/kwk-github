97ed1915c3e3d9857a22653c03b7dd21f92b8ad9
Zaira <zaira.r.bhatti@gmail.com>
2023-06-26 10:30:06 -0400

    Initial Commit

---
+++ kwk github/kwk_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  kwk_githubApp.swift
+//  kwk github
+//
+//  Created by Zaira Bhatti on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct kwk_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
